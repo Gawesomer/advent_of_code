3b2fc70a9f69c25c47ca6a5707f871c1619f90bb
Jean-Baptiste Jouband <jj.jouband@gmail.com>
2020-12-02 08:24:40 -0600

    Solve challenge-02 part 2

--- code/challenge_02.py
+++ code/challenge_02.py
@@ -12,29 +12,75 @@ def parse_line_from_file(input_file):
     return res
 
 
-def password_is_valid(policy_password):
+def parse_password_policy(policy_password):
     """
-    Returns whether or not given password is valid
-    Assumes password is a string formatted as such:
+    Parse and return information from password_policy
+    Assumes `policy_password` is a string formatted as such:
         "min-max letter: password"
     params:
-        password - string
+        policy_password - string
     returns:
-        True if `letter` appears at least `min` times and at most `max` times
-        in `password`, False otherwise
+        (min, max, letter, password)
+        min - integer
+        max - integer
+        letter - string
+        password - string
     """
     policy, password = policy_password.split(':')
+    password = password[1:]
     min_max, letter = policy.split(' ')
     min_count, max_count = min_max.split('-')
     min_count = int(min_count)
     max_count = int(max_count)
 
+    return (min_count, max_count, letter, password)
+
+
+def count_password_policy(policy_password):
+    """
+    Determine whether password is valid
+    Assumes `policy_password` is a string formatted as such:
+        "min-max letter: password"
+    params:
+        policy_password - string
+    returns:
+        True if `letter` appears at least `min` times and at most `max` times
+        in `password`, False otherwise
+    """
+    min_count, max_count, letter, password = parse_password_policy(
+                                                policy_password)
+
     letter_count = password.count(letter)
 
     return (min_count <= letter_count and letter_count <= max_count)
 
+def position_password_policy(policy_password):
+    """
+    Determine wheter password is valid
+    Assumes `policy_password` is a string formatted as such:
+        "min-max letter: password"
+    params:
+        policy_password - string
+    returns:
+        True if `letter` appears at position `min` xor at position `max` in
+        `password`, False otherwise
+        (Note: position is determined using 1-indexing)
+    """
+    min_count, max_count, letter, password = parse_password_policy(
+                                                policy_password)
 
-def num_invalid_passwords(passwords):
+    if len(password) < min_count:
+        return False
+    if password[min_count-1] == letter and password[max_count-1] == letter:
+        return False
+    if password[min_count-1] == letter:
+        return True
+    if password[max_count-1] == letter:
+        return True
+    return False
+
+
+def num_invalid_passwords(policy, policy_passwords):
     """
     Return number of invalid passwords
     Assumes each string given consists of a password policy and corresponding
@@ -46,8 +92,8 @@ def num_invalid_passwords(passwords):
         number of passwords in input that do not match their policy
     """
     res = 0
-    for line in passwords:
-        if password_is_valid(line):
+    for line in policy_passwords:
+        if policy(line):
             res += 1
     return res
 
@@ -55,4 +101,4 @@ def num_invalid_passwords(passwords):
 if __name__ == "__main__":
     with open("input_02.txt", "r") as input_file:
         lines = parse_line_from_file(input_file)
-    print(num_invalid_passwords(lines))
+    print(num_invalid_passwords(position_password_policy, lines))
--- tests/test_challenge_02.py
+++ tests/test_challenge_02.py
@@ -9,10 +9,19 @@ class TestProcessor(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_nominal_case(self):
+    def test_nominal_case_count_policy(self):
         lines = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]
+        policy = count_password_policy
+        valid_password_count = num_invalid_passwords(policy, lines)
 
-        self.assertEqual(num_invalid_passwords(lines), 2)
+        self.assertEqual(valid_password_count, 2)
+
+    def test_nominal_case_position_policy(self):
+        lines = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]
+        policy = position_password_policy
+        valid_password_count = num_invalid_passwords(policy, lines)
+
+        self.assertEqual(valid_password_count, 1)
 
 
 if __name__ == "__main__":
