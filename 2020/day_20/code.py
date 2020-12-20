fd977c47a66b8dae90f6be994d366c22613c1b23
Jean-Baptiste Jouband <jj.jouband@gmail.com>
2020-12-20 10:09:50 -0600

    2020 challenge-20 build id_map

--- 2020/day_20/code.py
+++ 2020/day_20/code.py
@@ -83,13 +83,84 @@ def all_matching(tile_id, tile, tiles):
     return res
 
 
+def get_tile_matching(tiles):
+    tile_to_matching = {}
+    for t_id, t in tiles.items():
+        tile_to_matching[t_id] = all_matching(t_id, t, tiles)
+    return tile_to_matching
+
+
+def is_edge(tile_id, tile_to_matching):
+    if len(tile_to_matching[tile_id]) <= 3:
+        return True
+    return False
+
+
+def get_edge_neighbours(tile_id, tile_to_matching):
+    res = set()
+    for t in tile_to_matching[tile_id]:
+        if is_edge(t, tile_to_matching):
+            res.add(t)
+    # print("{}: {}".format(tile_id, res))
+    return res
+
+
+def get_corners(tile_to_matching):
+    res = set()
+    for i, t in tile_to_matching.items():
+        if len(t) == 2:
+            res.add(i)
+    return res
+
+
+def get_adjacent_to_both(n1, n2, tile_to_matching):
+    for i, t in tile_to_matching.items():
+        if n1 in t and n2 in t:
+            return i
+    return None
+
+
+def build_id_map(tile_to_matching, width):
+    """
+    return:
+        list(list(tile_id))
+    """
+    res = [[0 for col in range(width)] for row in range(width)]
+
+    i = 0
+    while i < width:
+        j = 0
+        if i == 0:
+            curr = next(iter(get_corners(tile_to_matching)))
+        else:
+            curr = next(iter(get_edge_neighbours(res[i-1][0], tile_to_matching)))
+            tile_to_matching[res[i-1][0]].remove(curr)
+            tile_to_matching[curr].remove(res[i-1][0])
+        res[i][j] = curr
+        j += 1
+        while j < width:
+            if i == 0:
+                next_curr = next(iter(get_edge_neighbours(curr, tile_to_matching)))
+            else:
+                next_curr = get_adjacent_to_both(res[i][j-1], res[i-1][j], tile_to_matching)
+            tile_to_matching[curr].remove(next_curr)
+            tile_to_matching[next_curr].remove(curr)
+            curr = next_curr
+            res[i][j] = curr
+            j += 1
+        i += 1
+    return res
+
+
+
 if __name__ == "__main__":
     input_filename = os.path.join(pathlib.Path(__file__).parent, "input.txt")
     with open(input_filename, "r") as input_file:
         tiles = parse_input(input_file)
-    tile_to_matching = {}
-    for t_id, t in tiles.items():
-        tile_to_matching[t_id] = all_matching(t_id, t, tiles)
-    for t_id, s in tile_to_matching.items():
-        if len(s) == 2:
-            print(t_id)
+
+    tile_to_matching = get_tile_matching(tiles)
+    print(get_corners(tile_to_matching))
+
+    id_map = build_id_map(tile_to_matching.copy(), 12)
+    for row in id_map:
+        print(row)
