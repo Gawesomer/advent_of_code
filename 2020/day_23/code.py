628b3a33e42ec1eacda238bc6438cfdc21702d98
Jean-Baptiste Jouband <jj.jouband@gmail.com>
2020-12-23 14:14:57 -0600

    Solve 2020 challenge-23 part 2

--- 2020/day_23/code.py
+++ 2020/day_23/code.py
@@ -1,6 +1,7 @@
 import os
 import pathlib
 
+# Part1
 
 def insert_mod(i, elements, l, n):
     for e in elements:
@@ -47,10 +48,76 @@ def step(cups, curr_val):
 
     return cups[(curr_index+1)%len(cups)]
 
+# end Part1
+
+
+def serialize(cups):
+    """
+    return:
+        adjacency map: map[i] = cups.find(i-1)+1
+    """
+    res = [0 for e in cups]
+
+    prev = cups[0]
+    i = 1
+    while i < len(cups):
+        res[prev-1] = cups[i]
+        prev = cups[i]
+        i += 1
+    res[prev-1] = cups[0]
+
+    return res
+
+
+def next_element(seq_map, e):
+    return seq_map[e-1]
+
+
+def get_nth_element(seq_map, e, offset):
+    res = e
+    for i in range(offset):
+        res = next_element(seq_map, res)
+    return res
+
+
+def shuffle(seq_map, curr, highest):
+    to_move = [
+        get_nth_element(seq_map, curr, 1),
+        get_nth_element(seq_map, curr, 2),
+        get_nth_element(seq_map, curr, 3),
+    ]
+
+    dest = curr-1
+    if dest < 1:
+        dest = highest
+    while dest in to_move:
+        dest -= 1
+        if dest < 1:
+            dest = highest
+
+    seq_map[curr-1] = seq_map[to_move[2]-1]
+    tmp = seq_map[dest-1]
+    seq_map[dest-1] = to_move[0]
+    seq_map[to_move[2]-1] = tmp
+
+    return seq_map[curr-1]
+
+
 
 if __name__ == "__main__":
     cups = [1, 5, 8, 9, 3, 7, 4, 6, 2]
+    seq_map = serialize(cups)
+
     curr = 1
     for i in range(100):
-        curr = step(cups, curr)
-    print(cups)
+        curr = shuffle(seq_map, curr, 9)
+    print(seq_map)
+
+    for i in range(10, 1000001):
+        cups.append(i)
+
+    seq_map = serialize(cups)
+    curr = 1
+    for i in range(10000000):
+        curr = shuffle(seq_map, curr, 1000000)
+    print("{} * {}".format(seq_map[0], seq_map[seq_map[0]-1]))
--- 2020/day_23/test.py
+++ 2020/day_23/test.py
@@ -30,9 +30,41 @@ class TestProcessor(unittest.TestCase):
         curr = 3
         for i in range(10):
             curr = step(cups, curr)
-            print("{} {}".format(curr, cups))
 
         self.assertEqual(cups, expected_cups)
+
+    def test_serialize_nominal_case(self):
+        cups = [3, 8, 9, 1, 2, 5, 4, 6, 7]
+        expected_map = [2, 5, 8, 6, 4, 7, 3, 9, 1]
+
+        serialized = serialize(cups)
+
+        self.assertEqual(serialized, expected_map)
+
+    def test_get_nth_element_nominal_case(self):
+        seq_map = [2, 5, 8, 6, 4, 7, 3, 9, 1]
+
+        self.assertEqual(get_nth_element(seq_map, 3, 4), 2)
+
+    def test_shuffle_single_step(self):
+        seq_map = [2, 5, 8, 6, 4, 7, 3, 9, 1]
+        expected_cups = [3, 2, 8, 9, 1, 5, 4, 6, 7]
+        expected_seq_map = serialize(expected_cups)
+
+        self.assertEqual(shuffle(seq_map, 3, 9), 2)
+
+        self.assertEqual(seq_map, expected_seq_map)
+
+    def test_shuffle_ten_times(self):
+        seq_map = [2, 5, 8, 6, 4, 7, 3, 9, 1]
+        expected_cups = [5, 8, 3, 7, 4, 1, 9, 2, 6]
+        expected_seq_map = serialize(expected_cups)
+
+        curr = 3
+        for i in range(10):
+            curr = shuffle(seq_map, curr, 9)
+
+        self.assertEqual(seq_map, expected_seq_map)
 
 
 if __name__ == "__main__":
