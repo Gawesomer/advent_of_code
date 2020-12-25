606fd20ca6829070c19596f80ba94e941944b540
Jean-Baptiste Jouband <jj.jouband@gmail.com>
2020-12-24 23:29:07 -0600

    Solve 2020 challenge-25

---
+++ 2020/day_25/code.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    pass
+    """
+    Already implemented in other project
+    >>> shanks(7, 4206373, 20201227)
+    1245398
+    >>> sharedSecretDH(20201227, 1245398, 15113849)
+    1890859
+    """
---
+++ 2020/day_25/test.py
@@ -0,0 +1,20 @@
+import io
+import unittest
+
+from day_25.code import *
+
+
+class TestParser(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+
+class TestProcessor(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
