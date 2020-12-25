cef86f4903fda5901335c7d570f0644d78a8de02
Jean-Baptiste Jouband <jj.jouband@gmail.com>
2020-12-25 11:37:52 -0600

    Solve 2019 challenge-12 part 2

--- 2019/day_12/code.py
+++ 2019/day_12/code.py
@@ -54,9 +54,40 @@ def step(moons, n=1):
         applies gravity between moves and moves moons
     """
     for i in range(0, n):
+        print(i)
         apply_gravity(moons)
         for moon in moons:
             moon.move()
+
+
+def apply_gravity_single_axis(positions, velocities):
+    for i in range(len(positions)):
+        for j in range(len(positions)):
+            if positions[i] > positions[j]:
+                velocities[i] -= 1
+            elif positions[i] < positions[j]:
+                velocities[i] += 1
+
+
+def repeat_single_axis(positions):
+    velocities = [0 for pos in positions]
+    seen = [(tuple(positions), tuple(velocities))]
+
+    apply_gravity_single_axis(positions, velocities)
+    for i in range(len(positions)):
+        positions[i] += velocities[i]
+
+    counter = 1
+    while (tuple(positions), tuple(velocities)) not in seen:
+        #seen.append((tuple(positions), tuple(velocities)))
+        apply_gravity_single_axis(positions, velocities)
+        for i in range(len(positions)):
+            positions[i] += velocities[i]
+        counter += 1
+    print(positions)
+    print(velocities)
+    return counter
+    
 
 
 if __name__ == "__main__":
@@ -68,3 +99,25 @@ if __name__ == "__main__":
     for moon in moons:
         total += moon.total_energy
     print(total)
+
+    x_positions = [1, -1, 17, 12]
+    y_positions = [2, -9, 6, 4]
+    z_positions = [-9, -4, 8, 2]
+    print(repeat_single_axis(x_positions))
+    print(repeat_single_axis(y_positions))
+    print(repeat_single_axis(z_positions))
+    """
+    Implemented in another project
+    >>> gcd(186028, 167624)
+    4
+    >>> 186028*167624
+    31182757472
+    >>> 31182757472/4
+    7795689368
+    >>> gcd(7795689368, 193052)
+    4
+    >>> 7795689368*193052
+    1504973423871136
+    >>> 1504973423871136/4
+    376243355967784
+    """
