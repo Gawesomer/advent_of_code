22f564fc9782926bc9df86abbd77b3ed735caadc
Gawesomer <jean-baptiste@jouband.com>
2021-12-01 23:29:51 -0600

    Solve 2021 day 02 part 1

---
+++ 2021/day_02/code.lisp
@@ -0,0 +1,23 @@
+(defun get-input-lines ()
+  "Reads lines from file named input.txt"
+  (with-open-file (in "input.txt")
+    (loop for line = (read-line in nil nil)
+          while line
+          collect (let ((line-length (length line)))
+                    (list :direction (subseq line 0 (- line-length 2)) :delta (parse-integer (subseq line (1- line-length))))))))
+
+(defun navigate (directions)
+  (let ((horizontal 0) (depth 0))
+  (dolist (instruction directions)
+    (print instruction)
+    (cond
+      ((string-equal "up" (getf instruction :direction))
+       (setf depth (- depth (getf instruction :delta))))
+      ((string-equal "down" (getf instruction :direction))
+       (setf depth (+ depth (getf instruction :delta))))
+      ((string-equal "forward" (getf instruction :direction))
+       (setf horizontal (+ horizontal (getf instruction :delta))))))
+    (print horizontal)
+    (print depth)))
+
+(navigate (get-input-lines))
