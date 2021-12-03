c72ce3f47bc410e4cf13efd47cc118bcce857507
Gawesomer <jean-baptiste@jouband.com>
2021-12-03 00:46:20 -0600

    Solve 2021 day 3 part 2

--- 2021/day_03/code.lisp
+++ 2021/day_03/code.lisp
@@ -11,24 +11,47 @@
         (if (char= (elt line bitnum) #\0)
             (incf zero-count)
             (incf one-count)))
-      (if (> one-count zero-count)
-          (return-from most-common 1)
-          0)))
+      (if (>= one-count zero-count)
+          (return-from most-common #\1)
+          #\0)))
 
 (defun find-gamma (lines)
   (let ((res 0))
     (dotimes (i 12)
-      (if (eql (most-common lines (- 11 i)) 1)
+      (if (char= (most-common lines (- 11 i)) #\1)
           (setf res (+ res (expt 2 i)))))
     (print res)))
 
-(defun find-epsilon(lines)
+(defun find-epsilon (lines)
   (let ((res 0))
     (dotimes (i 12)
-      (if (eql (most-common lines (- 11 i)) 0)
+      (if (char= (most-common lines (- 11 i)) #\0)
           (setf res (+ res (expt 2 i)))))
     (print res)))
 
 ; 1616, 2479
 (find-gamma (get-input-lines))
 (find-epsilon (get-input-lines))
+
+(defun find-oxygen (lines)
+  (let ((common (most-common lines 0)))
+  (do ((filtered lines) (i 0))
+      ((eql i 11) filtered)
+    (print filtered)
+    (setf filtered (remove-if-not #'(lambda (line) (char= common (elt line i))) filtered))
+    (incf i)
+    (setf common (most-common filtered i)))))
+
+(defun find-carbon (lines)
+  (let ((common (most-common lines 0)))
+  (do ((filtered lines) (i 0))
+      ((eql i 11) filtered)
+    (print filtered)
+    (setf filtered (remove-if #'(lambda (line) (char= common (elt line i))) filtered))
+    (incf i)
+    (setf common (most-common filtered i)))))
+
+; 011000110011
+(print (find-oxygen (get-input-lines)))
+; 111110110010
+(print (find-carbon (get-input-lines)))
