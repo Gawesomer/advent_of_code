0e15c25940606fdec107fb0aaac232d7a3d91501
Gawesomer <jean-baptiste@jouband.com>
2021-12-06 23:40:27 -0600

    Solve 2021 day 7 part 2

--- 2021/day_07/code.lisp
+++ 2021/day_07/code.lisp
@@ -4,13 +4,21 @@
           while line
           collect (map 'list 'parse-integer (cl-utilities:split-sequence #\, line))))))
 
+;(defun fuel-to-target (target positions)
+;  (reduce #'+ positions :key #'(lambda (position) (abs (- position target)))))
+
+(defun geometric-sum (n)
+  (/ (* n (1+ n)) 2))
+
 (defun fuel-to-target (target positions)
-  (reduce #'+ positions :key #'(lambda (position) (abs (- position target)))))
+  (reduce #'+ positions :key #'(lambda (position) (geometric-sum (abs (- position target))))))
 
 (defun min-fuel (positions)
-  (do ((i (reduce #'min positions) (1+ i)) (contender (* (reduce #'max positions) (length positions))))
+  (do ((i (reduce #'min positions) (1+ i)) (contender 1000000000000)) ; Infinity?
       ((> i (reduce #'max positions)) contender)
+    (format t "~a: ~a~%" i (fuel-to-target i positions))
     (if (< (fuel-to-target i positions) contender)
         (setf contender (fuel-to-target i positions)))))
 
+;(print (min-fuel (list 16 1 2 0 4 2 7 1 2 14)))
 (print (min-fuel (parse-positions)))
