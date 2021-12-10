5d39a05340fb87600ab3ca0c2bcb0e08bf552ae5
Gawesomer <jean-baptiste@jouband.com>
2021-12-09 23:53:39 -0600

    Solve 2021 day 10 part 2

--- 2021/day_10/code.lisp
+++ 2021/day_10/code.lisp
@@ -10,7 +10,11 @@
     ((eql #\) bracket) #\()
     ((eql #\] bracket) #\[)
     ((eql #\> bracket) #\<)
-    ((eql #\} bracket) #\{)))
+    ((eql #\} bracket) #\{)
+    ((eql #\( bracket) #\))
+    ((eql #\[ bracket) #\])
+    ((eql #\< bracket) #\>)
+    ((eql #\{ bracket) #\})))
 
 (defun is-corrupted (line)
   (do ((i 0 (1+ i)) (stack (make-array (length line) :fill-pointer 0)))
@@ -38,3 +42,39 @@
     score))
 
 (print (corrupted-score (get-input-lines)))
+
+(defun complete-line (line)
+  (do ((i (1- (length line)) (1- i))
+       (stack (make-array (length line) :fill-pointer 0))
+       (to-add (make-array (length line) :fill-pointer 0)))
+      ((< i 0) to-add)
+    (if (or
+         (eql #\) (elt line i))
+         (eql #\] (elt line i))
+         (eql #\} (elt line i))
+         (eql #\> (elt line i)))
+        (vector-push (elt line i) stack)
+        (if (and (> (length stack) 0)
+                 (eql (elt stack (1- (length stack))) (matching-bracket (elt line i))))
+            (vector-pop stack)
+            (vector-push (matching-bracket (elt line i)) to-add)))))
+
+(defun completion-score (completion)
+  (let ((score 0))
+    (dotimes (i (length completion))
+      (setf score (* score 5))
+      (cond
+        ((eql #\) (elt completion i)) (incf score 1))
+        ((eql #\] (elt completion i)) (incf score 2))
+        ((eql #\} (elt completion i)) (incf score 3))
+        ((eql #\> (elt completion i)) (incf score 4))))
+  score))
+
+(defun completion-scores (lines)
+  (let ((scores NIL))
+    (dolist (line lines)
+      (push (completion-score (complete-line line)) scores))
+    scores))
+
+(print (sort (completion-scores (remove-if #'is-corrupted (get-input-lines))) #'<))
+(print (elt (sort (completion-scores (remove-if #'is-corrupted (get-input-lines))) #'<) 25))
