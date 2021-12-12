5c4991e275f838638d6b6b659e0e7344f332451a
Gawesomer <jean-baptiste@jouband.com>
2021-12-12 01:08:54 -0600

    Solve 2021 day 12 part 2

--- 2021/day_12/code.lisp
+++ 2021/day_12/code.lisp
@@ -20,7 +20,7 @@
 (defun neighbours (position graph)
   (gethash position graph))
 
-(defun non-visited-neighbours (position visited graph)
+(defun visitable-neighbours (position visited graph)
   (remove-if #'(lambda (e) (and (is-small-cave e) (find e visited :test #'equal))) (neighbours position graph)))
 
 (defun is-small-cave (position)
@@ -31,9 +31,33 @@
   (if (equal position "end")
       (return-from num-paths 1))
   (let ((res 0))
-    (dolist (neighbour (non-visited-neighbours position visited graph))
+    (dolist (neighbour (visitable-neighbours position visited graph))
       (incf res (num-paths neighbour (concatenate 'list (list position) visited) graph)))
     (format t "out: ~a, ~a~%" position res)
     res))
 
+(defparameter *joker-used* NIL)
+
+(defun visitable-neighbours-with-jokers (position visited graph)
+  (if *joker-used*
+      (remove-if #'(lambda (e) (and (is-small-cave e) (find e visited :test #'equal))) (neighbours position graph))
+      (remove-if #'(lambda (e) (or (equal e "start") (equal e "end"))) (neighbours position graph))))
+
+(defun num-paths-with-jokers (position visited graph)
+  (format t "in: ~a, joker: ~a~%" position *joker-used*)
+  (if (equal position "end")
+      (return-from num-paths-with-jokers 1))
+  (let ((res 0))
+    (dolist (neighbour (visitable-neighbours-with-jokers position visited graph))
+      (if (and (not *joker-used*) (is-small-cave neighbour) (find neighbour visited :test #'equal))
+          (progn
+            (setf *joker-used* T)
+            (incf res (num-paths-with-jokers neighbour (concatenate 'list (list position) visited) graph))
+            (setf *joker-used* NIL))
+          (incf res (num-paths-with-jokers neighbour (concatenate 'list (list position) visited) graph))))
+    (format t "out: ~a, ~a~%" position res)
+    res))
+
 (print (num-paths "start" NIL (build-adjacency-list (get-input-lines))))
+(print (num-paths-with-jokers "start" NIL (build-adjacency-list (get-input-lines))))
+(print (+ (num-paths "start" NIL (build-adjacency-list (get-input-lines))) (num-paths-with-jokers "start" NIL (build-adjacency-list (get-input-lines)))))
